7d008d6f1884d23afaee955519fe9745a6ea85a8
tdog <161662619+tdogpog@users.noreply.github.com>
2025-01-26 20:00:29 -0600

    Router improvements

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,9 @@ import Layout from "./components/Layout";
 const backend = "http://localhost:3000/";
 
 const router = createBrowserRouter([
+  //landing page that should explain the premise of the website
+  // and how they can interact with
+  //users need guidance or its just gonna look like nothing
   {
     path: "/",
     element: (
@@ -15,14 +18,8 @@ const router = createBrowserRouter([
       </Layout>
     ),
   },
-  {
-    path: "/auth",
-    element: (
-      <Layout>
-        <Login backend={backend} />
-      </Layout>
-    ),
-  },
+  //users will first LIKELY go here
+  //they will see the top games of the website
   {
     path: "/charts",
     element: (
@@ -33,19 +30,55 @@ const router = createBrowserRouter([
   },
   {
     path: "/games",
-    element: (
-      <Layout>
-        <Games backend={backend} />
-      </Layout>
-    ),
+    children: [
+      //specific game page /games/123
+      {
+        path: ":gameID",
+        element: (
+          <Layout>
+            <GameDetail backend={backend} />
+          </Layout>
+        ),
+      },
+      //genres page  /games/genres
+      {
+        path: "genres",
+        element: (
+          <Layout>
+            <Genres backend={backend} />
+          </Layout>
+        ),
+      },
+      //specific genre page /games/genres/456
+      {
+        path: "genres/:genreID",
+        element: (
+          <Layout>
+            <GenreGames backend={backend} />
+          </Layout>
+        ),
+      },
+    ],
   },
+  //user submit and edit tickets
   {
-    path: "/charts",
+    path: "/submit",
     element: (
       <Layout>
         <Submit backend={backend} />
       </Layout>
     ),
+    //makes it /submit/queue
+    children: [
+      {
+        path: "queue",
+        element: (
+          <Layout>
+            <Queue backend={backend} />
+          </Layout>
+        ),
+      },
+    ],
   },
 ]);
 createRoot(document.getElementById("root")).render(
