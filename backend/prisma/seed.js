e318c0e346a821aa2305bbabdb304a6b00d5631a
petermgoh <45682969+petermgoh@users.noreply.github.com>
2025-01-28 18:22:36 -0600

    Seeding File Added
    1. basic seeding file implemented.
    1.5. sample user, game, and rating
    2. local prisma DB up and running

---
+++ backend/prisma/seed.js
@@ -0,0 +1,44 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      username: 'john_doe',
+      email: 'john@example.com',
+      password: 'securepassword',
+    },
+  });
+
+  const game = await prisma.game.create({
+    data: {
+      title: 'Game Title',
+      releaseDate: new Date(),
+      coverImage: 'url_to_image',
+      developer: 'Game Dev Studio',
+      avgRating: 4.5,
+      reviewCount: 0,
+      ratingCount: 0,
+      approved: true,
+    },
+  });
+
+  await prisma.rating.create({
+    data: {
+      score: 5,
+      userID: user.id,
+      gameID: game.id,
+    },
+  });
+
+  console.log('Database seeded');
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
